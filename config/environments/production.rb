a5e0bcc40f4ba4a1b48b9ea307e33f53e92f664c
randolrs <r.shane.randolph@gmail.com>
2017-05-18 12:43:09 -0400

    diff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,13 +5,15 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
+    :storage => :s3,
+    :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
+
+config.action_mailer.default_url_options = { host: 'http://pinteresting-commits.herokuapp.com/' }
 
 
   # # General Settings
