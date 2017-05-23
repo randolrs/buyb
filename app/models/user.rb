f0e32ea868010a134617a49e2a5a3cb1f83ee7f0
randolrs <r.shane.randolph@gmail.com>
2017-05-23 08:45:06 -0400

    blame em

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,6 @@ class User < ActiveRecord::Base
 
  after_create :send_admin_mail
 
- after_create :send_simple_message
-  
-
   def send_admin_mail
 
     UserMailer.send_welcome_message_to_user(self).deliver
@@ -19,14 +16,6 @@ class User < ActiveRecord::Base
   end
 
 
-  def send_simple_message
-    RestClient.post "https://api:key-a95c47f4d450dd505a41737d4ceace9b"
-        "@api.mailgun.net/v3/sandbox6d94932b4dd342f1a283d5c0a34d0c0a.mailgun.org/messages",
-        :from => "Mailgun Sandbox <postmaster@sandbox6d94932b4dd342f1a283d5c0a34d0c0a.mailgun.org>",
-        :to => "Shane Randolph <r.shane.randolph@gmail.com>",
-        :subject => "Hello Shane Randolph",
-        :text => "Congratulations Shane Randolph, you just sent an email with Mailgun!  You are truly awesome!"
-	end
 
 
 end
