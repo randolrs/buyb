1a8f316785e4a272a47731233650f99b81258a3e
randolrs <r.shane.randolph@gmail.com>
2017-06-02 11:09:04 -0400

    change migration

--- db/migrate/20170602135211_add_parent_category_id_to_offer_categories.rb
+++ db/migrate/20170602135211_add_parent_category_id_to_offer_categories.rb
@@ -1,5 +1,5 @@
 class AddParentCategoryIdToOfferCategories < ActiveRecord::Migration[5.0]
   def change
-    add_column :offer_categories, :parent_category_id, :id
+    add_column :offer_categories, :parent_category_id, :integer
   end
 end
