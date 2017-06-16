2a46073eb1ba9e1f39e7c03cfb04aad069768aef
randolrs <r.shane.randolph@gmail.com>
2017-06-16 10:23:36 -0400

    not an act

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ApplicationMailer
 	default from: "Shane@thatHQ.com"
 
 	def send_welcome_message_to_user(user)
-		@user = user
-		mail(:to => @user.email, :subject => "Welcome!")
+		# @user = user
+		# mail(:to => @user.email, :subject => "Welcome!")
 	end
 
 end
